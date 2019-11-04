bd6483eb096f2a3c1373d14470996c4b90e0f857
AbdurNawaz <abdurnawaz.2011@gmail.com>
2019-11-04 21:33:34 +0530

    Add files via upload

---
+++ dqn.py
@@ -0,0 +1,50 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+
+env = gym.make('LunarLander-v2')
+
+from dqn_agent import Agent
+
+agent = Agent(state_size=8, action_size=4, seed=0)
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+
+    scores = []                        
+    scores_window = deque(maxlen=100)  
+    eps = eps_start                    
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env.render()
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       
+        scores.append(score)              
+        eps = max(eps_end, eps_decay*eps) 
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
